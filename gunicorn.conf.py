cd9102e01ef06f481cc3f171615e70a473482f4d
x0937952700-cmd <x0937952700@gmail.com>
2026-05-01 03:31:23 +0800

    Delete gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,9 +0,0 @@
-import os
-
-bind = "0.0.0.0:" + os.environ.get("PORT", "10000")
-workers = int(os.environ.get("WEB_CONCURRENCY", "1"))
-timeout = int(os.environ.get("GUNICORN_TIMEOUT", "120"))
-keepalive = int(os.environ.get("GUNICORN_KEEPALIVE", "5"))
-loglevel = os.environ.get("GUNICORN_LOG_LEVEL", "info")
-accesslog = "-"
-errorlog = "-"
