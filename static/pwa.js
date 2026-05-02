30db3a7309645870acded0ef5cabfa2184cba222
x0937952700-cmd <x0937952700@gmail.com>
2026-05-02 13:18:10 +0800

    Delete static directory

--- static/pwa.js
+++
@@ -1,5 +0,0 @@
-// CLEAN ROUTER V13: unregister old PWA caches.
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(regs => regs.forEach(r => r.unregister()));
-}
-if (window.caches) caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
--- static/yx_modules/yx_clean_other.js
+++
@@ -1,1 +0,0 @@
-(function(){'use strict'; window.loadShippingRecords=window.loadShippingRecords||function(){yxToast('此頁已停用舊版自動重畫，請回出貨完成後查詢資料庫紀錄');};})();
