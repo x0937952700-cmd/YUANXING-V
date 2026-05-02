e881cc844df3176172a204ead8c8337813f7529f
x0937952700-cmd <x0937952700@gmail.com>
2026-05-02 23:07:49 +0800

    Delete static directory

--- static/service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE_VERSION = 'full-master-v15-real-customer-direct-dom';
-self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request, {cache:'no-store'}).catch(() => fetch(event.request))); });
