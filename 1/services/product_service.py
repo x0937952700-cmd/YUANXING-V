23159c5072e2ec46cc4ff44f80ec5baa822c8096
x0937952700-cmd <x0937952700@gmail.com>
2026-05-10 23:16:42 +0800

    Delete 1 directory

--- 1/services/product_service.py
+++
@@ -1,9 +0,0 @@
-"""Product list, sorting, snapshots and transfer-facing helpers.
-
-Mainfile-safe facade. Functions remain in app.py/db.py for compatibility; new work should attach through this service name.
-"""
-
-SERVICE_NAME = "product_service"
-
-def marker():
-    return SERVICE_NAME
--- 1/static/yx_pages/orders_page.js
+++
@@ -1,2 +0,0 @@
-import './product_page_core.js?v=119';
-window.YX = window.YX || {}; window.YX.products = window.YX.products || {}; window.YX.products.page = 'orders';
