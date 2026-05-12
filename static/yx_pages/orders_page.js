2b8f712a326cbd41e93044000df4fb49de8bcfb9
x0937952700-cmd <x0937952700@gmail.com>
2026-05-12 08:22:27 +0800

    Delete static directory

--- static/yx_pages/orders_page.js
+++
@@ -1,4 +0,0 @@
-window.YX = window.YX || {};
-window.YX.products = window.YX.products || {};
-window.YX.products.page = 'orders';
-import('./product_page_core.js?v=119-v332-five-pack-shortcut-target-highlight');
