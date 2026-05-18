4961bcfa9415e15e9018b80948918279c725cd0c
x0937952700-cmd <x0937952700@gmail.com>
2026-05-18 10:59:15 +0800

    Delete 1 directory

--- 1/static/yx_pages/master_order_page.js
+++
@@ -1,5 +0,0 @@
-window.YX = window.YX || {};
-window.YX.products = window.YX.products || {};
-window.YX.products.page = 'master_order';
-const __yxProductCoreVersion = encodeURIComponent(window.__YX_STATIC_VERSION__ || '119-v406-warehouse-order-drag-longpress-fix');
-import(`./product_page_core.js?v=${__yxProductCoreVersion}`);
